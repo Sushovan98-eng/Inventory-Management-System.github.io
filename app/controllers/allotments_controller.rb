e84978599e93c97470d0181cee637bcb67d85d36
Sushovan98-eng <ssovan965@gmail.com>
2022-06-03 11:21:56 +0530

    change some controllers

--- app/controllers/allotments_controller.rb
+++ app/controllers/allotments_controller.rb
@@ -24,8 +24,8 @@ class AllotmentsController < ApplicationController
     def create
       @allotment = Allotment.new(allotment_params)
       if Allotment.exists?(user_id: @allotment.user_id, item_id: @allotment.item_id, dealloted_at: nil)
-        flash[:warning] =  "This Item is already alloted to this user."
-        redirect_to new_allotment_path
+        flash.now[:warning] =  "This Item is already alloted to this user."
+        render :new, status: :unprocessable_entity
       elsif @allotment.save
         successful_stock_update
         flash[:notice] = "Allotment made successfully."
