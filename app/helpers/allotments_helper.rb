fb1eeef2b7c23ffb544440d73681bab5fcf85669
Sushovan98-eng <ssovan965@gmail.com>
2022-06-06 18:51:54 +0530

    change some helpers

--- app/helpers/allotments_helper.rb
+++ app/helpers/allotments_helper.rb
@@ -17,8 +17,14 @@ module AllotmentsHelper
 
     def item_quantity_validation
       if item_id.present?
-          item = Item.find(item_id)
-          if item.in_stock < allotment_quantity.to_i
+        item = Item.find(item_id)
+          if Allotment.where(user_id: self.user_id, item_id: self.item_id, dealloted_at: nil).exists?
+            previous_quantity = Allotment.where(user_id: self.user_id, item_id: self.item_id).first.allotment_quantity
+            total_stock = item.in_stock + previous_quantity
+            if allotment_quantity.to_i > previous_quantity && total_stock < allotment_quantity.to_i
+              errors.add(:allotment_quantity, " is not sufficient for this allotment.")
+            end
+          elsif item.in_stock < allotment_quantity.to_i
           errors.add(:allotment_quantity, "is more than the available stock")
           end
       end
