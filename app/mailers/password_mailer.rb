c1efce20cc99087340ab0ab68046598060774f78
Sushovan98-eng <ssovan965@gmail.com>
2022-06-23 10:56:16 +0530

    change some controller

--- app/mailers/password_mailer.rb
+++ app/mailers/password_mailer.rb
@@ -2,7 +2,6 @@ class PasswordMailer < ApplicationMailer
   
 
     def reset
-       # @token = params[:user].signed_id(purpose: "password_reset", expires_in: 15.minutes)
         @token = params[:token]
         mail to: params[:user].email
     end
