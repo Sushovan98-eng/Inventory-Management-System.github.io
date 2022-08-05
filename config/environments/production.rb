1d93018785e2a83ad35b2b92f6ad25b03cad3454
Sushovan98-eng <ssovan965@gmail.com>
2022-08-05 16:26:59 +0530

    change some controller

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'herokuapp.com' , :protocol => 'https'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
