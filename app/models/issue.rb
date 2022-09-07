5f1581990d8c49695fa298b63ed1474b7ba1b034
Sushovan98-eng <ssovan965@gmail.com>
2022-09-07 15:29:23 +0530

    change some view

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -9,4 +9,18 @@ class Issue < ApplicationRecord
   default_scope { order(created_at: :desc) }
 
   scope :user_issues, -> { where(user_id: Current.user.id) }
+  
+  scope :solved_status_search, -> (search) do
+    if search == "solved"
+      where("solved_at IS NOT NULL")
+    elsif search == "unsolved"
+      where("solved_at IS NULL")
+    end
+  end
+  
+
+  def self.ransackable_scopes(auth_object = nil)
+    [:solved_status_search]
+  end  
+
 end
--- spec/models/issue_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Issue, type: :model do
-
-
-  
-  
-end
